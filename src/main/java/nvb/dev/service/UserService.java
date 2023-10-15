373012eabc7202bedd08e13000f4dff3b26a8460
Erfan Navab <erfannavab@gmail.com>
2023-10-16 00:17:13 +0330

    implement user service

--- src/main/java/nvb/dev/service/UserService.java
+++ src/main/java/nvb/dev/service/UserService.java
@@ -3,5 +3,18 @@ package nvb.dev.service;
 import nvb.dev.base.service.BaseService;
 import nvb.dev.entity.User;
 
+import java.util.Optional;
+
 public interface UserService extends BaseService<Long, User> {
+
+    void signUp(User user);
+
+    boolean isValid(User user);
+
+    Optional<User> findUserByFirstName(String firstName);
+
+    Optional<User> findUserByUsername(String username);
+
+    Optional<User> findUserByAge(int age);
+
 }
--- src/main/java/nvb/dev/service/impl/UserServiceImpl.java
+++ src/main/java/nvb/dev/service/impl/UserServiceImpl.java
@@ -1,10 +1,19 @@
 package nvb.dev.service.impl;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import nvb.dev.base.service.impl.BaseServiceImpl;
 import nvb.dev.entity.User;
+import nvb.dev.exception.ValidationException;
 import nvb.dev.repository.UserRepository;
 import nvb.dev.service.UserService;
 import org.hibernate.Session;
+import org.hibernate.validator.messageinterpolation.ParameterMessageInterpolator;
+
+import java.util.Optional;
+import java.util.Set;
 
 public class UserServiceImpl extends BaseServiceImpl<Long, User, UserRepository> implements UserService {
 
@@ -13,5 +22,54 @@ public class UserServiceImpl extends BaseServiceImpl<Long, User, UserRepository>
     public UserServiceImpl(Session session, UserRepository repository) {
         super(session, repository);
         this.session = session;
+    }
+
+    @Override
+    public void signUp(User user) {
+        session.getTransaction().begin();
+        repository.save(user);
+        session.getTransaction().commit();
+    }
+
+    @Override
+    public boolean isValid(User user) {
+        try {
+
+            ValidatorFactory validatorFactory = Validation.byDefaultProvider()
+                    .configure()
+                    .messageInterpolator(new ParameterMessageInterpolator())
+                    .buildValidatorFactory();
+
+            Validator validator = validatorFactory.getValidator();
+
+            Set<ConstraintViolation<User>> violationSet = validator.validate(user);
+            if (!violationSet.isEmpty()) {
+                for (ConstraintViolation<User> violation : violationSet) {
+                    System.out.println(violation.getMessage());
+                }
+                validatorFactory.close();
+                return false;
+            } else {
+                return true;
+            }
+        } catch (ValidationException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public Optional<User> findUserByFirstName(String firstName) {
+        return repository.findUserByFirstName(firstName);
+    }
+
+    @Override
+    public Optional<User> findUserByUsername(String username) {
+        return repository.findUserByUsername(username);
+    }
+
+    @Override
+    public Optional<User> findUserByAge(int age) {
+        return repository.findUserByAge(age);
     }
 }
