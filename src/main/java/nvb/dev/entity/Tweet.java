c3980a5926815c79aca3cc3969f0f562b2fc8874
Erfan Navab <erfannavab@gmail.com>
2023-10-16 00:16:45 +0330

    add max constraint for message

--- src/main/java/nvb/dev/entity/Tweet.java
+++ src/main/java/nvb/dev/entity/Tweet.java
@@ -1,6 +1,7 @@
 package nvb.dev.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import nvb.dev.base.entity.BaseEntity;
@@ -17,6 +18,7 @@ import java.util.Set;
 @ToString
 public class Tweet extends BaseEntity<Long> {
 
+    @Max(value = 280, message = "tweet cannot be more than 280 characters")
     private String message;
 
     @ManyToOne
