9b9edd3ff0a0ab9473a0f13837ae387e842b7625
Erfan Navab <erfannavab@gmail.com>
2023-10-16 21:58:16 +0330

    add and implement isValid method for tweet

--- src/main/java/nvb/dev/service/TweetService.java
+++ src/main/java/nvb/dev/service/TweetService.java
@@ -2,6 +2,8 @@ package nvb.dev.service;
 
 import nvb.dev.base.service.BaseService;
 import nvb.dev.entity.Tweet;
+import nvb.dev.entity.User;
 
 public interface TweetService extends BaseService<Long, Tweet> {
+    boolean isValid(Tweet tweet);
 }
--- src/main/java/nvb/dev/service/impl/TweetServiceImpl.java
+++ src/main/java/nvb/dev/service/impl/TweetServiceImpl.java
@@ -1,10 +1,19 @@
 package nvb.dev.service.impl;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import nvb.dev.base.service.impl.BaseServiceImpl;
 import nvb.dev.entity.Tweet;
+import nvb.dev.entity.User;
+import nvb.dev.exception.ValidationException;
 import nvb.dev.repository.TweetRepository;
 import nvb.dev.service.TweetService;
 import org.hibernate.Session;
+import org.hibernate.validator.messageinterpolation.ParameterMessageInterpolator;
+
+import java.util.Set;
 
 public class TweetServiceImpl extends BaseServiceImpl<Long, Tweet, TweetRepository> implements TweetService {
 
@@ -13,5 +22,32 @@ public class TweetServiceImpl extends BaseServiceImpl<Long, Tweet, TweetReposito
     public TweetServiceImpl(Session session, TweetRepository repository) {
         super(session, repository);
         this.session = session;
+    }
+
+    @Override
+    public boolean isValid(Tweet tweet) {
+        try {
+
+            ValidatorFactory validatorFactory = Validation.byDefaultProvider()
+                    .configure()
+                    .messageInterpolator(new ParameterMessageInterpolator())
+                    .buildValidatorFactory();
+
+            Validator validator = validatorFactory.getValidator();
+
+            Set<ConstraintViolation<Tweet>> violationSet = validator.validate(tweet);
+            if (!violationSet.isEmpty()) {
+                for (ConstraintViolation<Tweet> violation : violationSet) {
+                    System.out.println(violation.getMessage());
+                }
+                validatorFactory.close();
+                return false;
+            } else {
+                return true;
+            }
+        } catch (ValidationException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
 }
